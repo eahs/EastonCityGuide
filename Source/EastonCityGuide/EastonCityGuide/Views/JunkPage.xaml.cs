3ddd206af783690fdc380a6decef7e6cb487be41
Tajvir Chahal <tajvirchahal87@gmail.com>
2019-09-24 11:30:38 -0400

    tanczos demonstration

---
+++ Source/EastonCityGuide/EastonCityGuide/Views/JunkPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace EastonCityGuide.Views
+{
+    public partial class JunkPage : ContentPage
+    {
+        public JunkPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
