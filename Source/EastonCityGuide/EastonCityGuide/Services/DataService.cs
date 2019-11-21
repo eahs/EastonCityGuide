8dcef1ed3d8d9b0ecc32654fa528889fdf21aba4
ADD <lizard1255@gmail.com>
2019-11-21 10:25:39 -0500

    Search Bar Works
    The Search Bar now goes to the location of where you type, but only if you type it exactly

--- Source/EastonCityGuide/EastonCityGuide/Services/DataService.cs
+++ Source/EastonCityGuide/EastonCityGuide/Services/DataService.cs
@@ -1,5 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using EastonCityGuide.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using EastonCityGuide.Services;
 
 namespace EastonCityGuide.Services
 {
@@ -10,14 +14,29 @@ namespace EastonCityGuide.Services
            "Center Square",
            "Christopher Columbus Statue",
            "Easton-PBurg Bridge",
-           "Easton Public Libary",
+           "Easton Public Library",
            "Easton Public Market",
            "Larry Holmes Statue",
            "NorthHampton County Court House",
            "Nurture Nature Center",
            "RiverSide Park",
            "Sigal Museum",
-           "State Theater"
+           "State Theater",
+        };
+
+        public static List<Position> Locations { get; } = new List<Position>
+        {
+            new Position(40.691216, -75.209130),
+            new Position(40.692109, -75.205205),
+            new Position(40.694538, -75.203715),
+            new Position(40.691944, -75.213611),
+            new Position(40.691281, -75.210421),
+            new Position(40.689864, -75.205397),
+            new Position(40.687306, -75.217623),
+            new Position(40.690585, -75.213675),
+            new Position(40.692191, -75.205048),
+            new Position(40.690848, -75.210674),
+            new Position(40.691389, -75.212500),
         };
 
         public static List<string> GetSearchResults(string queryString)
