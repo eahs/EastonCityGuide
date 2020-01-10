78bf159832f429f1d0d2e637044f7c10d5270e4a
Jennie Duong <jennieduong5@gmail.com>
2020-01-10 12:27:56 -0500

    Login Page Reformatted
    has logo and works

---
+++ Source/EastonCityGuide/EastonCityGuide/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace EastonCityGuide.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public User() { }
+        public User(string Username, string Password)
+        {
+            this.Username = Username;
+            this.Password = Password;
+        }
+
+        public bool CheckInformation()
+        {
+            if (this.Username.Equals("TOES") && this.Password.Equals("toes"))
+                return true;
+            else
+                return false;
+        }
+    }
+}
+
