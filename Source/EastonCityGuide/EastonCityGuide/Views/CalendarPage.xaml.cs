b931dd5ae80b4dea749d5a56e0e0526274882825
Jennie Duong <jennieduong5@gmail.com>
2020-01-30 10:27:37 -0500

    Added Scheduler Nuget Package and created CalenarPage

---
+++ Source/EastonCityGuide/EastonCityGuide/Views/CalendarPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Syncfusion.SfSchedule.XForms;
+using Xamarin.Forms;
+
+namespace GettingStarted
+{
+    public class App : Application
+    {
+        SfSchedule schedule;
+        public App()
+        {
+            InitializeComponent();
+            //Creating new instance for SfSchedule   
+            schedule = new SfSchedule();
+            MainPage = new ContentPage { Content = schedule };
+        }
+    }
+}
